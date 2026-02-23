d05d6f4f9eb0bfae866dede1c53777d07bdbf983
Tabbykat049 <156952207+Tabbykat049@users.noreply.github.com>
2026-02-23 15:40:57 -0700

    Initial commit

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void Play()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has Quit The Game");
+    }
+}
